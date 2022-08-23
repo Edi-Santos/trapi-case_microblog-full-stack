d04b6becacdc32f74dbcae089d69ce38f104c306
Ed Santos <edsantos2822@gmail.com>
2022-08-23 05:38:40 -0300

    feat:(authorization.js): implementando middleware de autenticação

---
+++ back-end/src/auth/authorization.js
@@ -0,0 +1,27 @@
+const JWT = require('jsonwebtoken');
+
+const secret = 'meuPastelEMaisBarato';
+const jwtConfig = { expiresIn: '1h', algorithm: 'HS256' };
+
+const authorization = (req, res, next) => {
+  const token = req.headers.Authorization;
+
+  try {
+    if (!token) return res.status(401).json({ message: 'token not found' });
+
+    const decoded = JWT.verify(token, secret);
+    const { data } = decoded;
+
+    req.user = data;
+    next();
+  } catch (err) {
+    console.log(`Erro no Authorization || ${err}`);
+    return res.status(400).json({ message: 'invalid token' });
+  }
+};
+
+module.exports = {
+  secret,
+  jwtConfig,
+  authorization,
+};
