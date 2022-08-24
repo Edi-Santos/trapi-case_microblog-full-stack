e6a76d0c9df2f4a30117d8e3327117fce74d1e15
Ed Santos <edsantos2822@gmail.com>
2022-08-23 23:24:38 -0300

    build: implementando validação para o endpoint POST /user

--- back-end/src/validations/userValidations/userValidation.js
+++ back-end/src/validations/userValidations/userValidation.js
@@ -1,8 +1,21 @@
 const schema = require('./loginDatasSchema');
+const createSchema = require('./createDatasSchema');
 const errorMessage = require('../errorMessages');
 
 const datasValidationLogin = (loginDatas) => {
   const { error } = schema.validate(loginDatas);
+
+  if (error) {
+    const { message } = error.details[0];
+
+    return { status: 400, message };
+  }
+
+  return true;
+};
+
+const datasValidationCreate = (userDatas) => {
+  const { error } = createSchema.validate(userDatas);
 
   if (error) {
     const { message } = error.details[0];
@@ -21,5 +34,6 @@ const userNotFound = (user) => {
 
 module.exports = {
   datasValidationLogin,
+  datasValidationCreate,
   userNotFound,
 };
