bb8b258441bd584b113f73b55564a666d7179923
Ed Santos <edsantos2822@gmail.com>
2022-08-25 13:17:54 -0300

    test:(getPosts.test.js): adicionando testes ao endpoint GET /post

---
+++ back-end/tests/getPosts.test.js
@@ -0,0 +1,72 @@
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const sinon = require('sinon');
+
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const server = require('../src/api/app');
+
+chai.use(chaiHTTP);
+
+const { expect } = chai;
+
+describe('GET /post', () => {
+  describe.only('Testa quando a requisição é bem sucedida', () => {
+    let response = {};
+    let DBServer;
+    
+    before(async () => {
+      DBServer = await MongoMemoryServer.create();
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(URLMock,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      // Cria usuário
+      await chai.request(server)
+        .post('/user')
+        .send({
+          "name": "Eren Yeager",
+          "email": "eren@email.com",
+          "password": "@1eren1@",
+        })
+
+      const login = await chai.request(server)
+        .post('/login')
+        .send({
+          "email": "eren@email.com",
+          "password": "@1eren1@",
+        })
+
+      const { token } = login.body;
+      
+      response = await chai.request(server)
+        .get('/post')
+        .set({
+          "Authorization": token,
+        })
+        .send({});
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    })
+
+    it('retorna o código de status 200', () => {
+      expect(response).to.have.status(200);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.a('object');
+    });
+
+    it('o objeto possui a propriedade "posts"', () => {
+      expect(response.body).to.have.property('posts');
+    });
+  });
+});
