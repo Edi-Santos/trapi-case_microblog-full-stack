0d915af23872a886cf5cb899b6db2252dbf61da6
Ed Santos <edsantos2822@gmail.com>
2022-08-25 12:30:32 -0300

    test:(login.test.js): adicionando mais casos de teste ao endpoint POST /login

--- back-end/tests/login.test.js
+++ back-end/tests/login.test.js
@@ -100,4 +100,66 @@ describe('POST /login', () => {
       expect(response.body).to.have.property('message');
     });
   });
+
+  describe('Testa quando algum campo não é informado ou é inválido', () => {
+    let response1 = {};
+    let response2 = {};
+    let response3 = {};
+    let DBServer;
+
+    before(async () => {
+      DBServer = await MongoMemoryServer.create();
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(URLMock,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      response1 = await chai.request(server)
+        .post('/login')
+        .send({
+          "email": "",
+          "password": "@1eren1@",
+        });
+
+        response2 = await chai.request(server)
+        .post('/login')
+        .send({
+          "email": "erenemail.com",
+          "password": "@1eren1@",
+        });
+
+        response3 = await chai.request(server)
+        .post('/login')
+        .send({
+          "email": "eren@email.com",
+          "password": "",
+        });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    })
+
+    it('retorna o código de status 400', () => {
+      expect(response1).to.have.status(400);
+      expect(response2).to.have.status(400);
+      expect(response3).to.have.status(400);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response1.body).to.be.a('object');
+      expect(response2.body).to.be.a('object');
+      expect(response3.body).to.be.a('object');
+    });
+
+    it('o objeto possui a propriedade "message"', () => {
+      expect(response1.body).to.have.property('message');
+      expect(response2.body).to.have.property('message');
+      expect(response3.body).to.have.property('message');
+    });
+  });
 });
