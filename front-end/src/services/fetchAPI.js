adcb74c1ae135306fe2643eb5f128df7ba914624
Ed Santos <edsantos2822@gmail.com>
2022-08-26 00:54:17 -0300

    build:(fetchAPI.js): implementando função request()

---
+++ front-end/src/services/fetchAPI.js
@@ -0,0 +1,12 @@
+const request = (url, method, headers, body) => {
+  const options = {
+    method,
+    headers,
+    body,
+  };
+
+  return fetch(url, options)
+    .then((response) => response.json());
+}
+
+module.exports = request;
