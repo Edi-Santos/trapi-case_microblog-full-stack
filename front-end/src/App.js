4fb797e9caf2eecb6f643095eddeb6cf14c80b69
Ed Santos <edsantos2822@gmail.com>
2022-08-26 11:45:20 -0300

    feat:(App.js): adicionando rota para página de feed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,11 +4,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 
 import './App.css';
+import Feed from './pages/feed/Feed';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
+      <Route path="/feed" element={ <Feed /> } />
       <Route path="/" element={ <Navigate replace to="/login" /> } />
     </Routes>
   );
