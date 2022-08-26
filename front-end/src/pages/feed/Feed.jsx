623a1bccee182efb6a8c65b537f0f9bbf6c53495
Ed Santos <edsantos2822@gmail.com>
2022-08-26 15:39:43 -0300

    build:(Feed.jsx): implementando tela de feed

---
+++ front-end/src/pages/feed/Feed.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from 'react';
+import PostsList from '../../components/feed/PostsList';
+import MyContext from '../../contextAPI/MyContext';
+
+import request from '../../services/fetchAPI';
+
+function Feed() {
+  const { token, setAllPosts } = useContext(MyContext);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const url = 'http://localhost:3001/post';
+      const method = 'GET';
+      const headers = {
+        'Content-type': 'application/json; charset=utf8',
+        Authorization: token,
+      };
+
+      const { posts } = await request(url, method, headers);
+
+      setAllPosts(() => ({ posts }));
+    };
+
+    getPosts();
+  }, [token, setAllPosts]);
+
+  return (
+    <main>
+      <PostsList />
+    </main>
+  );
+}
+
+export default Feed;
